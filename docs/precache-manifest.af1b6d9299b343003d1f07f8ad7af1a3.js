9631033aeb833576ac3ac38cbbbca395f4ffb560
Juan Pablo Tello <juan.tello@smg.com.ar>
2020-06-29 01:32:18 -0300

    Docs agregado para github

---
+++ docs/precache-manifest.af1b6d9299b343003d1f07f8ad7af1a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9067b11d3dbcb4d9dc04a7149631d844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cb084decc7ea14f30a4",
+    "url": "/static/css/main.2e064222.chunk.css"
+  },
+  {
+    "revision": "2b4521626756fd8651f7",
+    "url": "/static/js/2.70300478.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.70300478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cb084decc7ea14f30a4",
+    "url": "/static/js/main.fc73daa1.chunk.js"
+  },
+  {
+    "revision": "26d7a32ebf461b85d192",
+    "url": "/static/js/runtime-main.c7b0ba44.js"
+  },
+  {
+    "revision": "7dfbff8bab986da21aca6999f55f9157",
+    "url": "/static/media/Yo.7dfbff8b.jpg"
+  }
+]);
