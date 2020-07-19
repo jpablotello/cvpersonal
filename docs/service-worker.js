e715be02ff6944c756c16cb5bc1e380c0dce9b11
Juan Pablo Tello <juan.tello@smg.com.ar>
2020-07-19 17:35:23 -0300

    Correccion estados educacion

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.514dae57b99e2a1aa0e802d24f509429.js"
+  "/precache-manifest.48696b355695fe14dbb9fb573bf2fa6e.js"
 );
 
 self.addEventListener('message', (event) => {
