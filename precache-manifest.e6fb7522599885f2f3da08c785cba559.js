79b48eb3af4bf0be44b74fe8bfbcac6c9d729ab1
Juan Pablo Tello <juan.tello@smg.com.ar>
2020-07-20 22:11:40 -0300

    Updates

---
+++ precache-manifest.e6fb7522599885f2f3da08c785cba559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4ab78d70e821545d4694c1ded4a898",
+    "url": "/cvpersonal/index.html"
+  },
+  {
+    "revision": "fecbdd7dc49b99436383",
+    "url": "/cvpersonal/static/css/main.2e064222.chunk.css"
+  },
+  {
+    "revision": "3830039924f31a998ed1",
+    "url": "/cvpersonal/static/js/2.de1c76de.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cvpersonal/static/js/2.de1c76de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecbdd7dc49b99436383",
+    "url": "/cvpersonal/static/js/main.0aadfbe9.chunk.js"
+  },
+  {
+    "revision": "a64ab50e33c434cedb7e",
+    "url": "/cvpersonal/static/js/runtime-main.d38d2411.js"
+  },
+  {
+    "revision": "7dfbff8bab986da21aca6999f55f9157",
+    "url": "/cvpersonal/static/media/yo.7dfbff8b.JPG"
+  }
+]);
