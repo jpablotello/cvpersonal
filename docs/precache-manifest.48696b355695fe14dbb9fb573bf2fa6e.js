0e3c9cf126fd580580aebdb857d2f7493109fbac
Juan Pablo Tello <juan.tello@smg.com.ar>
2020-07-20 21:18:25 -0300

    Actualizacion data

---
+++ docs/precache-manifest.48696b355695fe14dbb9fb573bf2fa6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1303975d27929f01836db086250ab203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fecbdd7dc49b99436383",
+    "url": "/static/css/main.2e064222.chunk.css"
+  },
+  {
+    "revision": "3830039924f31a998ed1",
+    "url": "/static/js/2.de1c76de.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.de1c76de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecbdd7dc49b99436383",
+    "url": "/static/js/main.0aadfbe9.chunk.js"
+  },
+  {
+    "revision": "26d7a32ebf461b85d192",
+    "url": "/static/js/runtime-main.c7b0ba44.js"
+  },
+  {
+    "revision": "7dfbff8bab986da21aca6999f55f9157",
+    "url": "/static/media/yo.7dfbff8b.JPG"
+  }
+]);
